8a6ee4824178fecbe0c89c615cbad7e0749c3131
Rhea Myers <rhea@myers.studio>
2019-12-08 20:33:16 -0800

    Initial revision.

---
+++ generate-qr-code.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+QR_CODE_FILENAME="shared-secret-qr-code.svg"
+ISSUER="Shared%20Secret%20%282020%29"
+ID="Rob%20Myers"
+SECRET="$(echo $1 | sha256sum | cut -f 1 -d ' ' | xxd -r -p - | base32 | cut -f 1 -d '=')"
+AUTH="otpauth://totp/${ID}?secret=${SECRET}&issuer=${ISSUER}"
+
+if [ -f "${QR_CODE_FILENAME}" ]; then
+    echo "QR Code file exists: ${QR_CODE_FILENAME}"
+    echo "Not overwriting."
+    exit 0
+fi
+
+qrencode -t SVG --size 10 --margin 0 --output "${QR_CODE_FILENAME}" "${AUTH}"
